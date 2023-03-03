f0ca06254e920dd134e2cfa203f6ae6a455985ac
Nijin P S <nijingithub@gmail.com>
2023-03-03 18:14:49 +0530

    More problems

--- Star 1-> Star 2/Basic Programming/CSTOCK.java
+++ Star 1-> Star 2/Basic Programming/CSTOCK.java
@@ -2,4 +2,35 @@
  * Link to problem : https://www.codechef.com/LP1TO201/problems/CSTOCK
  */
 
- 
+ /* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-- > 0){
+		    int S = sc.nextInt();
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    int C = sc.nextInt();
+		    
+		    double chPrice = ((S*C)/(100*1.0))+S;
+		    
+		    if(chPrice >= A && chPrice <= B){
+		        System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
---
+++ Star 1-> Star 2/Basic Programming/MXEVNSUB.java
@@ -0,0 +1,33 @@
+/*
+ * Link to problem : 
+ */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-- > 0){
+		    int N = sc.nextInt();
+		    
+		    int sum = (N*(N+1))/2;
+		    
+		    if(sum%2 == 0){
+		        System.out.println(N);
+		    }
+		    else{
+		        System.out.println(N-1);
+		    }
+		}
+	}
+}
