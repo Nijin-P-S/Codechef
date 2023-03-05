1673be53367a2ff0a50e85a6c92ec18f95a2cd6d
Nijin P S <nijingithub@gmail.com>
2023-03-05 12:35:15 +0530

    More problems

---
+++ Star 1-> Star 2/Time Complexity/CM164364.java
@@ -0,0 +1,37 @@
+/*
+ * Link to problem : 
+ */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int x = sc.nextInt();
+		    
+		    HashSet<Integer> unique = new HashSet<>();
+		    for(int i=0; i<n; i++){
+		        unique.add(sc.nextInt());
+		    }
+		    if(n-unique.size()>=x){
+		        System.out.println(unique.size());
+		    }
+		    else{
+		        System.out.println(unique.size()-(x-(n-unique.size())));
+		    }
+		}
+	}
+}
