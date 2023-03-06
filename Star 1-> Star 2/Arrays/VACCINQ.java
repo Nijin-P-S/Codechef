3a21f7a5a7d3a7c5ddf27f0fe65182116faba48b
Nijin P S <nijingithub@gmail.com>
2023-03-06 17:11:08 +0530

    More problems

---
+++ Star 1-> Star 2/Arrays/VACCINQ.java
@@ -0,0 +1,39 @@
+/*
+ * Link to problem : 
+ */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int N = sc.nextInt();
+		    int P = sc.nextInt();
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    
+		    int sum = 0;
+		    for(int i=1; i<=N; i++){
+		        int elem = sc.nextInt();
+		        if(i > P)
+		            continue;
+		        else if(elem == 0)
+		            sum+=X;
+		        else if(elem == 1)
+		            sum+=Y;
+		    }
+		    System.out.println(sum);
+		}
+	}
+}
--- Star 1-> Star 2/Time Complexity/GFTSHP.java
+++ Star 1-> Star 2/Time Complexity/GFTSHP.java
@@ -1,5 +1,5 @@
 /*
- * Link to problem : 
+ * Link to problem : https://www.codechef.com/LP1TO205/problems/GFTSHP
  */
 
 /* package codechef; // don't place package name! */
